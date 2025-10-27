030c7ee3ca8616b16cc045e5e6de8c02d7f7ca96
Artem P <artmp24@yandex.ru>
2025-10-27 18:30:10 +0300

    complete 2nd step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.53.0"
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
