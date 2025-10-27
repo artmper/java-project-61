8881cd8b29af0b7401b82872ad649387b676b718
Artem P <artmp24@yandex.ru>
2025-10-27 18:43:44 +0300

    complete 3rd step

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetingUser() {
+        Scanner scanner = new Scanner(System.in);
+        String userName = "";
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        userName = scanner.nextLine();
+
+        System.out.println("Hello, " + userName + "!");
+    }
+}
