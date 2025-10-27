7d136b1e0f3b8a5e72212d2f45bf6a15042545c8
Artem P <artmp24@yandex.ru>
2025-10-27 18:58:29 +0300

    add sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.53.0"
     application
+    id("org.sonarqube") version "7.0.1.6134"
 }
 
 group = "hexlet.code"
@@ -27,3 +28,9 @@ tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
 
+sonar {
+    properties {
+        property("sonar.projectKey", "artmper_java-project-61")
+        property("sonar.organization", "artmper")
+    }
+}
