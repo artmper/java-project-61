6f27698633bc1457c11b7ef31b37b5051bc19e40
Artem P <artmp24@yandex.ru>
2025-10-27 20:00:10 +0300

    add checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
     id("com.github.ben-manes.versions") version "0.53.0"
     application
     id("org.sonarqube") version "7.0.1.6134"
+    checkstyle
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 application { mainClass.set("hexlet.code.App") }
+checkstyle {
+    toolVersion = "10.12.4"
+}
 
 repositories {
     mavenCentral()
